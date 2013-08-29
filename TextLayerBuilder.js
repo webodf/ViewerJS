546cc61b5b172f03e15a2a01d04f4b39e4a654f0
Tobias Hintze <th-git@kogmbh.com>
2013-08-29 15:06:02 +0200

    added missing copyright header

--- TextLayerBuilder.js
+++ TextLayerBuilder.js
@@ -1,3 +1,21 @@
+/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* Copyright 2012 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// adapted for Viewer.js by KO GmbH
+
 /**
  * TextLayerBuilder provides text-selection
  * functionality for the PDF. It does this
