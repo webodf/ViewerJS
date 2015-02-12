0027ce5e6f2e8596cd6c4e8659dd1970bc634e79
Friedrich W. H. Kossebau <friedrich@kogmbh.com>
2015-02-12 18:49:15 +0100

    Update to WebODF 0.5.5

--- PluginLoader.js
+++ PluginLoader.js
@@ -1,6 +1,5 @@
 /**
- * @license
- * Copyright (C) 2012 KO GmbH <copyright@kogmbh.com>
+ * Copyright (C) 2012-2015 KO GmbH <copyright@kogmbh.com>
  *
  * @licstart
  * The JavaScript code in this page is free software: you can redistribute it
@@ -38,54 +37,190 @@
 
 /*global document, window, Viewer, ODFViewerPlugin, PDFViewerPlugin*/
 
-var viewer;
-
-function loadPlugin(pluginName, callback) {
-    "use strict";
-    var script, style;
-
-    // Load script
-    script = document.createElement('script');
-    script.async = false;
-    script.onload = callback;
-    script.src = pluginName + '.js';
-    script.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-function loadDocument(documentUrl) {
+function loadDocument() {
     "use strict";
 
-    if (documentUrl) {
-        var extension = documentUrl.split('.').pop(),
-            Plugin;
-        extension = extension.toLowerCase();
+    var pluginRegistry = [
+        (function() {
+            var odfMimetypes = [
+                'application/vnd.oasis.opendocument.text',
+                'application/vnd.oasis.opendocument.text-flat-xml',
+                'application/vnd.oasis.opendocument.text-template',
+                'application/vnd.oasis.opendocument.presentation',
+                'application/vnd.oasis.opendocument.presentation-flat-xml',
+                'application/vnd.oasis.opendocument.presentation-template',
+                'application/vnd.oasis.opendocument.spreadsheet',
+                'application/vnd.oasis.opendocument.spreadsheet-flat-xml',
+                'application/vnd.oasis.opendocument.spreadsheet-template'];
+            var odfFileExtensions = [
+                'odt',
+                'fodt',
+                'ott',
+                'odp',
+                'fodp',
+                'otp',
+                'ods',
+                'fods',
+                'ots'];
 
-        switch (extension) {
-        case 'odt':
-        case 'fodt':
-        case 'ott':
-        case 'odp':
-        case 'fodp':
-        case 'otp':
-        case 'ods':
-        case 'fods':
-        case 'ots':
-            loadPlugin('./ODFViewerPlugin', function () {
-                Plugin = ODFViewerPlugin;
-            });
-            break;
-        case 'pdf':
-            loadPlugin('./PDFViewerPlugin', function () {
-                Plugin = PDFViewerPlugin;
-            });
-            break;
+            return {
+                supportsMimetype: function(mimetype) {
+                    return (odfMimetypes.indexOf(mimetype) !== -1);
+                },
+                supportsFileExtension: function(extension) {
+                    return (odfFileExtensions.indexOf(extension) !== -1);
+                },
+                path: "./ODFViewerPlugin",
+                getClass: function() { return ODFViewerPlugin; }
+            };
+        }()),
+        {
+            supportsMimetype: function(mimetype) {
+                return (mimetype === 'application/pdf');
+            },
+            supportsFileExtension: function(extension) {
+                return (extension === 'pdf');
+            },
+            path: "./PDFViewerPlugin",
+            getClass: function() { return PDFViewerPlugin; }
         }
+    ];
 
-        window.onload = function () {
-            if (Plugin) {
-                viewer = new Viewer(new Plugin());
+    function loadPlugin(pluginName, callback) {
+        "use strict";
+        var script, style;
+
+        // Load script
+        script = document.createElement('script');
+        script.async = false;
+        script.onload = callback;
+        script.src = pluginName + '.js';
+        script.type = 'text/javascript';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+
+
+    function estimateTypeByHeaderContentType(documentUrl, cb) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            var mimetype, matchingPluginData;
+            if (xhr.readyState === 4) {
+                if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 0) {
+                    mimetype = xhr.getResponseHeader('content-type');
+
+                    if (mimetype) {
+                        pluginRegistry.some(function(pluginData) {
+                            if (pluginData.supportsMimetype(mimetype)) {
+                                matchingPluginData = pluginData;
+                                console.log('Found plugin by mimetype and xhr head: ' + mimetype);
+                                return true;
+                            }
+                            return false;
+                        });
+                    }
+                }
+                cb(matchingPluginData);
             }
         };
+        xhr.open("HEAD", documentUrl, true);
+        xhr.send();
     }
+
+
+    function doEstimateTypeByFileExtension(extension) {
+        var matchingPluginData;
+
+        pluginRegistry.some(function(pluginData) {
+            if (pluginData.supportsFileExtension(extension)) {
+                matchingPluginData = pluginData;
+                return true;
+            }
+            return false;
+        });
+
+        return matchingPluginData;
+    }
+
+
+    function estimateTypeByFileExtension(extension) {
+        var matchingPluginData = doEstimateTypeByFileExtension(extension)
+
+        if (matchingPluginData) {
+            console.log('Found plugin by parameter type: ' + extension);
+        }
+
+        return matchingPluginData;
+    }
+
+
+    function estimateTypeByFileExtensionFromPath(documentUrl) {
+        // See to get any path from the url and grep what could be a file extension
+        var documentPath = documentUrl.split('?')[0],
+            extension = documentPath.split('.').pop(),
+            matchingPluginData = doEstimateTypeByFileExtension(extension)
+
+        if (matchingPluginData) {
+            console.log('Found plugin by file extension from path: ' + extension);
+        }
+
+        return matchingPluginData;
+    }
+
+
+    function parseSearchParameters(location) {
+        var parameters = {},
+            search = location.search || "?";
+
+        search.substr(1).split('&').forEach(function (q) {
+            // skip empty strings
+            if (!q) {
+                return;
+            }
+            // if there is no '=', have it handled as if given key was set to undefined
+            var s = q.split('=', 2);
+            parameters[decodeURIComponent(s[0])] = decodeURIComponent(s[1]);
+        });
+
+        return parameters;
+    }
+
+
+    window.onload = function () {
+        var viewer,
+            documentUrl = document.location.hash.substring(1),
+            parameters = parseSearchParameters(document.location),
+            Plugin;
+
+        if (documentUrl) {
+            // try to guess the title as filename from the location, if not set by parameter
+            if (!parameters.title) {
+                parameters.title = documentUrl.replace(/^.*[\\\/]/, '');
+            }
+
+            parameters.documentUrl = documentUrl;
+
+            // trust the server most
+            estimateTypeByHeaderContentType(documentUrl, function(pluginData) {
+                if (!pluginData) {
+                    if (parameters.type) {
+                        pluginData = estimateTypeByFileExtension(parameters.type);
+                    } else {
+                        // last ressort: try to guess from path
+                        pluginData = estimateTypeByFileExtensionFromPath(documentUrl);
+                    }
+                }
+
+                if (pluginData) {
+                    loadPlugin(pluginData.path, function () {
+                        Plugin = pluginData.getClass();
+                        viewer = new Viewer(new Plugin(), parameters);
+                   });
+                } else {
+                    viewer = new Viewer();
+                }
+            });
+        } else {
+            viewer = new Viewer();
+        }
+    };
 }
