c489dc706a58ca8cd429dbd14bb8611a46742ede
Friedrich W. H. Kossebau <friedrich@kogmbh.com>
2015-04-16 16:18:32 +0200

    Import ViewerJS source files from WebODF
    files from WebODF:programs/viewer, 9de1f20bbf9d76ae3c754fe52fd97ea98bf68875
    
    Also:
    * minimize all JS files with ClosureCompiler
    * mention ViewerJS version in About dialog

---
+++ HeaderCompiled.js
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * This is a generated file. DO NOT EDIT.
+ *
+ * Copyright (C) 2012-2015 KO GmbH <copyright@kogmbh.com>
+ *
+ * @licstart
+ * This file is part of the compiled version of the ViewerJS module.
+ *
+ * ViewerJS is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License (GNU AGPL)
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * ViewerJS is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with ViewerJS.  If not, see <http://www.gnu.org/licenses/>.
+ * @licend
+ *
+ * @source: http://viewerjs.org/
+ * @source: http://github.com/kogmbh/ViewerJS
+ */
